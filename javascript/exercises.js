ebd7eed83f8ed753a65077ffebb2f852e1c38e42
Manuel Gonzalez <manugonzalez1992@gmail.com>
2023-03-13 20:52:24 +0100

    exercise1 js

---
+++ javascript/exercises.js
@@ -0,0 +1,13 @@
+let hugo = 0;
+
+let paco = 2 * hugo;
+
+let luis = paco + 10;
+
+total_monedas = hugo + paco + luis;
+
+if (total_monedas === 85){
+    console.log("Hugo: " + hugo);
+    console.log("paco: " + paco);
+    console.log("luis: " + luis);
+}
